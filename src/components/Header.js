c6d04e2cdf8352e2b1964f042b1f7e16798cd5e1
Arash Zarei <arashzarei.js@gmail.com>
2023-03-28 12:38:31 +0430

    created header and added classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+// Images
+import Logo from '../assets/logo.svg';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <header className='py-8'>
+    <div className="container mx-auto">
+      <div className='flex justify-between items-center'>
+        <a href="#">
+          <img src={Logo} alt="logo" />
+        </a>
+        <button className='btn btn-sm'>Work With Me</button>
+      </div>
+    </div>
+  </header>
+  )
 };
 
 export default Header;
