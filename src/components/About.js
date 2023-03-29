776d69431d0730b00ccedfe3cf5c4adaf2ad867c
Arash Zarei <arashzarei.js@gmail.com>
2023-03-29 12:00:38 +0430

    created about component and added classes and animation text and finished.

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,115 @@
-import React from 'react';
+import React from "react";
+
+// Contup
+import CountUp from "react-countup";
+// intersection
+import { InView, useInView } from "react-intersection-observer";
+// motion
+import { motion } from "framer-motion";
+// variant
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="section" id="about">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-10 lg:gap-y-0 h-screen">
+          {/* image */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
+          {/* text */}
+          <div>
+            <motion.h2
+              variants={fadeIn("left", 0.3)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.7 }}
+              className="h2 text-accent mb-4"
+            >
+              About me.
+            </motion.h2>
+            <motion.h3
+              variants={fadeIn("left", 0.3)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.7 }}
+              className="h3 mb-4"
+            >
+              I'm Front-end Developer with over 1 yers of experience.
+            </motion.h3>
+            <p className="mb-6">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptate velit cumque modi natus, ipsam aperiam.
+            </p>
+            {/* stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <motion.div
+                variants={fadeIn("up", 0.4)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.3 }}
+              >
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {InView ? <CountUp start={0} end={13} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br />
+                  Experience
+                </div>
+              </motion.div>
+              <motion.div
+                variants={fadeIn("up", 0.6)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.3 }}
+              >
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {InView ? <CountUp start={0} end={15} duration={3} /> : null}
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br />
+                  Compeleted
+                </div>
+              </motion.div>
+              <motion.div
+                variants={fadeIn("up", 0.8)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.3 }}
+              >
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {InView ? <CountUp start={0} end={12} duration={3} /> : null}
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Satisfied <br />
+                  Clients
+                </div>
+              </motion.div>
+            </div>
+            <motion.div
+              variants={fadeIn("down", 0.3)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex gap-x-8 items-center"
+            >
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
