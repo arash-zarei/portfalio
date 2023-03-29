6b4770d8c0b643dccb346bfe16d00d91ce7e732f
Arash Zarei <arashzarei.js@gmail.com>
2023-03-29 13:20:08 +0430

    created work component and added styles and classes and finished.

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,98 @@
-import React from 'react';
+import React from "react";
+
+// Motion
+import { motion } from "framer-motion";
+// Variant
+import { fadeIn } from "../variants";
+// Images
+import img1 from "../assets/portfolio-img1.png";
+import img2 from "../assets/portfolio-img2.png";
+import img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="section" id="work">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex flex-1 flex-col gap-y-12 mb-10 lg:mb-0"
+          >
+            {/* text */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                My Latest <br />
+                Work
+              </h2>
+              <p className="max-w-sm mb-16">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde
+                magnam quasi debitis! Numquam ipsum suscipit dolores tempore,
+                mollitia eius ullam!
+              </p>
+              <button className="btn btn-sm">View All Projects</button>
+            </div>
+            {/* image */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl:">
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={img1}
+                alt="work"
+              />
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">React Project</span>
+              </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Shoping Cart</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.4)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 gap-y-10 flex flex-col"
+          >
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl:">
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={img2}
+                alt="work"
+              />
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">React Project</span>
+              </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">News Web</span>
+              </div>
+            </div>
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl:">
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={img3}
+                alt="work"
+              />
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">React Project</span>
+              </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Blog Web</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
