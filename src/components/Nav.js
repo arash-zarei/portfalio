7f369c150e282f9a7f701355abdfdfe495bbb606
Arash Zarei <arashzarei.js@gmail.com>
2023-03-30 15:16:54 +0430

    fixed a bug scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -63,6 +63,7 @@ const Nav = () => {
             offset={-200}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
+            
             <BsChatSquareText />
           </Link>
         </div>
