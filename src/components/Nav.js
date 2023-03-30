05d931b930367575680d20d012ac56c44cdbbe70
Arash Zarei <arashzarei.js@gmail.com>
2023-03-30 15:24:26 +0430

    fixed a bug scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,7 +30,7 @@ const Nav = () => {
             activeClass="active"
             smooth={true}
             spy={true}
-            offset={-200}
+            offset={-60}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiUser />
@@ -40,7 +40,7 @@ const Nav = () => {
             activeClass="active"
             smooth={true}
             spy={true}
-            offset={-200}
+            offset={-60}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsClipboardData />
@@ -50,7 +50,7 @@ const Nav = () => {
             activeClass="active"
             smooth={true}
             spy={true}
-            offset={-200}
+            offset={-60}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsBriefcase />
@@ -60,10 +60,10 @@ const Nav = () => {
             activeClass="active"
             smooth={true}
             spy={true}
-            offset={-200}
+            offset={-60}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
-            
+
             <BsChatSquareText />
           </Link>
         </div>
